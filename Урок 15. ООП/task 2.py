f06e77a262c2ae2a8e48cc33886e4ffe400c494a
KlaUtron <vlad.zrelov@bk.ru>
2025-07-18 11:53:38 +0300

    Update task 2.py

--- Урок 15. ООП/task 2.py
+++ Урок 15. ООП/task 2.py
@@ -1,9 +1,18 @@
 class Transport:
-    def __init__(self, name, max_passengers):
+    def __init__(self, name, max_speed, mileage):
         self.name = name
-        self.max_passengers = max_passengers
+        self.max_speed = max_speed
+        self.mileage = mileage
         
-    
-autobus = Transport('Renaul Logan', 50)
+    def seating_capacity(self, capacity):
+        return  f"Вместимость одного автобуса {self.name}  {capacity} пассажиров"
 
-print(f"Вместимость одного автобуса {autobus.name}  {autobus.max_passengers} пассажиров")
+class Autobus(Transport):
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+    
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity)
+    
+renault_logan = Autobus("Renaul Logan", 120, 10000)
+print(renault_logan.seating_capacity())
